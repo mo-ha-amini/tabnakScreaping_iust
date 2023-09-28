6cc6d5f4d43e3646d9598f3b6d3ad93e57f824ff
Mohammad Hasan <mhaminii18@gmail.com>
2023-09-28 13:38:03 +0330

    small changes in scaper

--- scraping/main.py
+++ scraping/main.py
@@ -8,9 +8,11 @@ from datetime import datetime
   
 def getnewsLink():
     links = []
-    cart_id = [0,1,2,3,4,5,6,8,9,10,11,19,22,23,24,42,51,59,71,72,75,95,96,98,99,100,101,102,103,138,139,140,141,142,143,144,145,146,147,148,149,182,184,185,186,187,188,245]
+    # cart_id = [0,1,2,3,4,5,6,8,9,10,11,19,22,23,24,42,51,59,71,72,75,95,96,98,99,100,101,102,103,138,139,140,141,142,143,144,145,146,147,148,149,182,184,185,186,187,188,245]
+    cart_id = [1]
+    
     for id in tqdm(cart_id):
-        for page in range(0,2):
+        for page in range(0,1):
             URL = "https://www.tabnak.ir/fa/archive?service_id=1&sec_id=-1&cat_id="+ str(id) +"&rpp=20&from_date=1384/01/01&to_date=1402/07/04&p=" + str(page)
             r = requests.get(URL)
             soup = BeautifulSoup(r.content, 'html5lib')
@@ -40,6 +42,7 @@ def convertToDateTimeformat(persianDate, enDate):
 
 def scarpeEachLink(links):
     data = []
+    arrayOfText = []
     for link in tqdm(links):
         news = ()
         r = requests.get(link)
@@ -71,16 +74,19 @@ def scarpeEachLink(links):
             
             news+=(newsText,)
             
+            arrayOfText.append(newsText)
+
             shortlink =soup.find('input', {"id":"foo"})
             shortlink = shortlink['value']
             news+= (shortlink,)
 
         data.append(news)
 
-    with open('scrapedData.txt', 'w',  encoding='utf-8') as writer:
-            for row in data:
-                writer.write(str(row))
-                writer.write('\n')
+    with open('textArr.txt', 'w',  encoding='utf-8') as writer:
+            # for row in data:
+            #     writer.write(str(row))
+            #     writer.write('\n')
+        writer.write(str(arrayOfText))
 
 def main():
     links = getnewsLink()
